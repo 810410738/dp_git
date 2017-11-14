fb2a686756a3f22dbf162b537e39e80b8eac41b9
jerry_zj <jerrylzj810410738@gmail.com>
2017-11-14 14:38:32 +0800

    蛮力法修改正确

--- 资源优化问题.cpp
+++ 资源优化问题.cpp
@@ -5,11 +5,11 @@
 */
 /*
 假设5台设备分配给3个车间，c数组为[
-	1 2 3
-	2 7 3
-	4 5 1
-	3 4 8
-	8 4 2
+	1 2 1 
+	2 4 3
+	4 5 4 
+	5 6 8
+	8 7 9 
 ] 
 */
 #include<iostream>
@@ -29,38 +29,46 @@ void show(int **c,int n, int m){
 	}
 	cout<<endl;	
 }
-int cal(int **c, int **temp, int n, int m){
-	int sum= 0;
+int cal(int **c, int *temp, int n, int m){
+	int sum= 0; 
 	int i,j;
-	for(i=0;i<n;i++){
-		for(j=0;j<m;j++){
-			if(temp[i][j] == 1){
-			 sum+=c[i][j];	
-			}
-		}
+	for(j=1;j<=m;j++){
+		sum += c[temp[j]][j];
 	}
 	return sum;
 }
-void violence1(int**c,int ** temp,int n, int m,int i){
-	int j , result;
-	if(i>=n){
-//		show(temp,n,m);
+void violence1(int**c,int * temp,int n, int m,int j,int * Result){
+	int i , result;
+	if(j>=m){
+		temp[j] = n;
+		for(int k=1;k<m;k++){
+			temp[j] -= temp[k]; 
+		}
+		if(temp[j]<0){
+			return ;
+		}
+		//print
+		for(int k=1;k<=m;k++){
+			cout<<temp[k]<<" ";
+		}
+		cout<<endl;
 		result = cal(c,temp,n,m);
-		if(Max < result)
-		{
+		if(result > Max){
+			for(int k=1;k<=m;k++){
+				Result[k] = temp[k];
+		}
 			Max = result;
 		}
+		//遍历的总数加一 
 		count++;
 		return ;
 	}
-	for(j=0;j<m;j++){
-		//放
-		temp[i][j] = 1; 
-		violence1(c,temp,n,m,i+1);
-		temp[i][j] = 0; 
+	for(i=0;i<=n;i++){
+		//放的个数从0个到n个，j从1开始 
+		temp[j] = i; 
+		violence1(c,temp,n,m,j+1,Result);
 	}
 }
-
 //优化后的蛮力法，不用辅助数组 
 void violence2(int**c,int n, int m,int i){
 	int j ;
@@ -97,22 +105,30 @@ int dp(int**c,int n,int m,int i){
 	}
 	return result;
 } 
-dp_bottom_up(int**c,int n, int m){
-	int *r = new int[n+1];
-	r[0] = 0;
-	int i,j,result = 0;
-	for(i=1;i<=n;i++){
-		for(j=0;j<m;j++){
-//			result = max(result,c[i-1][j]+r[i-1]);
-			int temp ;
-			temp = c[i-1][j]+r[i-1];
-			if(temp>result){
-				result = temp;
-		}	 
-		}
-		r[i] = result;
+int dp_bottom_up(int**c,int n, int m){
+	int i,j,k,temp;
+	int **f = new int*[n+1];
+	for(i=0;i<=n;i++){
+		f[i] = new int[m+1];
 	}
-	return r[n];
+	for(j=0;j<m;j++){//车间数 
+		for(i=0;i<n;i++){//设备数
+			if(j == 0){
+				f[i][j] = c[i][j];
+			}
+			else{
+				for(k=0;k<i;k++)
+				{	
+					temp = f[k][j-1] + c[i-k][j];
+					if(f[i][j] < temp ){
+						f[i][j] = temp;
+					}
+				} 
+			}	
+		}
+	}
+	 
+	return f[n-1][m-1];
 }
 int main()
 {
@@ -122,33 +138,44 @@ int main()
 	int i,j;
 	int n,m;
 	cin>>n>>m;
-	int **c = new int*[n];
-	for(i=0;i<n;i++){
-		c[i] = new int[m];
+	int **c = new int*[n+1];
+	for(i=0;i<=n;i++){
+		c[i] = new int[m+1];
 	}
 	//input c array
 	//开始计时
 		c_start = clock();
-		for(i=0;i<n;i++){
-		for(j=0;j<m;j++){
-			c[i][j] = rand();
+		for(i=0;i<=n;i++){
+		for(j=0;j<=m;j++){
+			if(i == 0){
+				c[i][j] = 0;
+			}
+			else if(i == 1){
+				c[i][j] = rand();	
+			}
+			else{
+			    c[i][j] = c[i-1][j] + rand();	
+			}
+			if(j == 0){
+				c[i][j] = 0;
+			}
+			cout<<c[i][j]<<" ";
 //			cin>>c[i][j]; 
 		}
+		cout<<endl;
 	}
+	cout<<endl;
 		c_end = clock();
 		//结束计时
 		time = difftime(c_end, c_start);
 	cout << "生成随机数耗时："<<time << "ms "<<endl;
-//	//new a temp array
-//	int **temp = new int*[n];
-//	for(i=0;i<n;i++){
-//		temp[i] = new int[m];
-//	}
-//	for(i=0;i<n;i++){
-//		for(j=0;j<m;j++){
-//			temp[i][j] = 0;
-//		}
-//	}
+	//new a temp array
+	int *temp = new int[m+1];
+	int * Result = new int[m+1];
+	for(i=0;i<=m;i++){
+		temp[i] = 0;
+		Result[i] = 0;
+	} 
 	cout<<"***********************************************************"<<endl;
 	cout<<"dp自底向上："<<endl;
 	//开始计时
@@ -159,21 +186,25 @@ int main()
 		time = difftime(c_end, c_start);
 	cout << "dp自底向上耗时："<<time << "ms "<<endl;
 	cout<<"dp自底向上最大价值为:"<<result<<endl;
-	/*  
+	  
 	cout<<"***********************************************************"<<endl;
-	//violence(c,temp,n,m,0);
 	cout<<"蛮力法："<<endl;
 		//开始计时
 		c_start = clock();
-		violence2(c,n,m,0);	
+		violence1(c,temp,n,m,1,Result);	
 		c_end = clock();
 		//结束计时
 		time = difftime(c_end, c_start);
 	cout << "蛮力法耗时："<<time << "ms "<<endl;
+	cout<<"最大价值方案为："<<endl; 
+	for(i=1;i<=m;i++){
+		cout<<Result[i]<<" ";
+	} 
+	cout<<endl;
 	cout<<"最大价值为："<<Max<<endl;
-		cout<<"遍历次数："<<count<<endl;
+	cout<<"遍历次数："<<count<<endl;
 	cout<<"***********************************************************"<<endl;
-	
+	/*
 	count = 0;
 	cout<<"dp递归实现："<<endl;
 		//开始计时
